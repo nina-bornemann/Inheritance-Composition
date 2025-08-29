ca29ed779a82a08e5a48bbfa3d9e4a8cbf11a690
Nina Bornemann <nina.bornemann@freenet.de>
2025-08-29 03:52:17 -0700

    updated polymorphism

--- src/main/java/com/ninabornemann/Main.java
+++ src/main/java/com/ninabornemann/Main.java
@@ -27,6 +27,18 @@ public class Main {
 
         Vehicle[] vehicles = {vehicle1, car1, car2, bike1};
         System.out.println(Arrays.toString(vehicles));
+        String [] ExpManufacturers = {vehicle1.getManufacturer(),
+                car1.getManufacturer(),
+                car2.getManufacturer(),
+                bike1.getManufacturer()};
+        System.out.println(Arrays.toString(ExpManufacturers));
+
+        String[] manufacturers = new String[4];
+        for (int i = 0; i < vehicles.length; i++) {
+            manufacturers[i] = vehicles[i].getManufacturer();
+        }
+        System.out.println(Arrays.toString(manufacturers));
+
 
         // Composition Trains
 
